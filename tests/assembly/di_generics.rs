31fa7e1fb0a16d94b979a8a1f22bf5a49c22ace4
Quentin JEROME <qjerome@users.noreply.github.com>
2023-07-17 11:03:46 +0200

    added bool and char const generic testing
    Signed-off-by: Quentin JEROME <qjerome@users.noreply.github.com>

--- tests/assembly/di_generics.rs
+++ tests/assembly/di_generics.rs
@@ -34,7 +34,10 @@ pub fn connect() {
     let _ = my_function(5, 6);
     let _ = my_function(7, 8);
 
-    let _ = const_generic::<42>(1);
+    let _ = const_generic_usize::<42>(1);
+    let _ = const_generic_bool::<true>(42);
+    let _ = const_generic_bool::<false>(42);
+    let _ = const_generic_char::<'A'>(42);
 }
 
 pub trait Add<Rhs = Self> {
@@ -52,9 +55,27 @@ impl Add for u32 {
 }
 
 #[inline(never)]
-fn const_generic<const N: usize>(m: usize) -> usize {
+fn const_generic_usize<const N: usize>(m: usize) -> usize {
     let mut n = N;
     (0..512).for_each(|_| n += m);
+    n
+}
+
+#[inline(never)]
+fn const_generic_bool<const b: bool>(t: usize) -> usize {
+    let mut n = t;
+    if b {
+        (0..512).for_each(|_| n += 1);
+    } else {
+        (0..512).for_each(|_| n += 2);
+    }
+    n
+}
+
+#[inline(never)]
+fn const_generic_char<const c: char>(b: usize) -> usize {
+    let mut n = c as usize;
+    (0..512).for_each(|_| n += b);
     n
 }
 
@@ -78,4 +99,8 @@ pub fn my_function<T: Add<Output = T> + Copy>(x: T, y: T) -> T {
 // CHECK: name: "Foo_3C_u32_3E_"
 // CHECK: name: "Bar_3C_di_generics_3A__3A_Foo_3C_u32_3E__3E_"
 // CHECK: name: "my_function_3C_u32_3E_"
-// CHECK: name: "const_generic_3C_42_3E_"
+// CHECK: name: "const_generic_usize_3C_42_3E_"
+// CHECK: name: "const_generic_bool_3C_true_3E_"
+// CHECK: name: "const_generic_bool_3C_false_3E_"
+// name of const generic char does not seem to be predictable but we check it is here
+// CHECK: name: "{{const_generic_char_3C_[[:print:]]+_3E_}}"
